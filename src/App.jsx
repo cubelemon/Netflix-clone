fba7d343b6d9f92624e3c959e5e9799ee916a764
Natasha Png <natashapng.png@gmail.com>
2025-11-21 11:37:37 +1100

    navbar beasted

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Home from './pages/Home/Home.jsx'
+import Login from './pages/Login/Login.jsx'
+import Player from './pages/Player/Player.jsx'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Home/>
+
+
+    </>
   )
 }
 
